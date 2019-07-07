22337511cf74d9beb143b5989f5f7906f82a2611
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-07-07 16:24:10 -0400

    Polishing and next steps.

--- tests/src/org/nasdanika/codegen/tests/StaticTextTests.java
+++ tests/src/org/nasdanika/codegen/tests/StaticTextTests.java
@@ -16,7 +16,6 @@ import org.nasdanika.codegen.html.CodegenDocumentationGenerator;
 import org.nasdanika.codegen.util.ConfigurableValidatingModelGenerator;
 import org.nasdanika.codegen.util.ValidatingModelGenerator;
 import org.nasdanika.common.Context;
-import org.nasdanika.common.InterpolatingSource;
 import org.nasdanika.common.MutableContext;
 import org.nasdanika.common.PrintStreamProgressMonitor;
 import org.nasdanika.common.ProgressEntry;
@@ -36,6 +35,8 @@ public class StaticTextTests extends TestsBase {
 	
 	private static final String TEST_MODELS_BASE_URI = "org.nasdanika.codegen.tests.models/models/";
 	
+	// --- Basic ---
+	
 	/**
 	 * Generates a greeting without interpolation.
 	 * @throws Exception
@@ -49,17 +50,34 @@ public class StaticTextTests extends TestsBase {
 		mc.put("name", "World");
 		
 		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
-		ProgressEntry pe = new ProgressEntry("Generating Generator Model Documentation", 0);
+		ProgressEntry pe = new ProgressEntry("Generating basic text", 0);
 		List<String> result = validatingModelGenerator.execute(mc, progressMonitor.compose(pe));	
 		
 		// HTML report
 		Container<Object> container = fsc.adapt(null, encoder, null);
-		ProgressReportGenerator prg = new ProgressReportGenerator("Documentation generation", pe);
+		ProgressReportGenerator prg = new ProgressReportGenerator("Generation", pe);
 		prg.generate(container.getContainer("progress-report"), progressMonitor);				
 		
 		Assert.assertEquals(1, result.size());
 		Assert.assertEquals("Hello, ${name}!", result.get(0));
 	}
+		
+	@Test
+	public void testBasicDocumentationGeneration() throws Exception {
+		CodegenDocumentationGenerator generator = new CodegenDocumentationGenerator("Nasdanika Basic Codegen Model", null);
+		generator.loadModel(TEST_MODELS_BASE_URI+"static-text/basic.codegen");
+		Container<InputStream> fsc = new FileSystemContainer(new File("target/generator-model-doc/static-text/basic"));
+		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
+		ProgressEntry pe = new ProgressEntry("Generating static text", 0);
+		Container<Object> container = fsc.adapt(null, encoder, null);
+		generator.generate(container, progressMonitor.compose(pe));
+		
+		// HTML report
+		ProgressReportGenerator prg = new ProgressReportGenerator("Generation", pe);
+		prg.generate(container.getContainer("progress-report"), progressMonitor);		
+	}
+	
+	// --- Interpolation ---
 	
 	/**
 	 * Generates a greeting with interpolation.
@@ -74,12 +92,12 @@ public class StaticTextTests extends TestsBase {
 		mc.put("greetings/name", "World");
 		
 		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
-		ProgressEntry pe = new ProgressEntry("Generating Generator Model Documentation", 0);
+		ProgressEntry pe = new ProgressEntry("Generating interpolated static text", 0);
 		List<String> result = validatingModelGenerator.execute(mc, progressMonitor.compose(pe));	
 		
 		// HTML report
 		Container<Object> container = fsc.adapt(null, encoder, null);
-		ProgressReportGenerator prg = new ProgressReportGenerator("Documentation generation", pe);
+		ProgressReportGenerator prg = new ProgressReportGenerator("Generation", pe);
 		prg.generate(container.getContainer("progress-report"), progressMonitor);				
 		
 		Assert.assertEquals(1, result.size());
@@ -99,18 +117,17 @@ public class StaticTextTests extends TestsBase {
 		mc.put("greetings", Collections.singletonMap("name", "World"));
 		
 		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
-		ProgressEntry pe = new ProgressEntry("Generating Generator Model Documentation", 0);
+		ProgressEntry pe = new ProgressEntry("Generating interpolated text", 0);
 		List<String> result = validatingModelGenerator.execute(mc, progressMonitor.compose(pe));	
 		
 		// HTML report
 		Container<Object> container = fsc.adapt(null, encoder, null);
-		ProgressReportGenerator prg = new ProgressReportGenerator("Documentation generation", pe);
+		ProgressReportGenerator prg = new ProgressReportGenerator("Generation", pe);
 		prg.generate(container.getContainer("progress-report"), progressMonitor);				
 		
 		Assert.assertEquals(1, result.size());
 		Assert.assertEquals("Hello, World!", result.get(0));
 	}
-
 	
 	/**
 	 * Generates a greeting with interpolation and a property hierarchy retrieved from a map wrapped into a context.
@@ -127,13 +144,13 @@ public class StaticTextTests extends TestsBase {
 		mc.register(Container.class, fsc);
 		
 		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
-		ProgressEntry pe = new ProgressEntry("Generating Generator Model Documentation", 0);
+		ProgressEntry pe = new ProgressEntry("Generating interpolated text", 0);
 		Context context = mc.compose(Context.wrap(config::get));
 		List<String> result = validatingModelGenerator.execute(context, progressMonitor.compose(pe));	
 		
 		// HTML report
 		Container<Object> container = fsc.adapt(null, encoder, null);
-		ProgressReportGenerator prg = new ProgressReportGenerator("Documentation generation", pe);
+		ProgressReportGenerator prg = new ProgressReportGenerator("Generation", pe);
 		prg.generate(container.getContainer("progress-report"), progressMonitor);				
 		
 		Assert.assertEquals(1, result.size());
@@ -174,19 +191,18 @@ public class StaticTextTests extends TestsBase {
 		Container<InputStream> fsc = new FileSystemContainer(new File("target/generator-tests/static-text/interpolation-with-configuration"));
 		
 		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
-		ProgressEntry pe = new ProgressEntry("Generating Generator Model Documentation", 0);
+		ProgressEntry pe = new ProgressEntry("Generating interpolated text", 0);
 		List<String> result = generator.bindContext(Context.EMPTY_CONTEXT).execute(progressMonitor.compose(pe));	
 		
 		// HTML report
 		Container<Object> container = fsc.adapt(null, encoder, null);
-		ProgressReportGenerator prg = new ProgressReportGenerator("Documentation generation", pe);
+		ProgressReportGenerator prg = new ProgressReportGenerator("Generation", pe);
 		prg.generate(container.getContainer("progress-report"), progressMonitor);				
 		
 		Assert.assertEquals(1, result.size());
 		Assert.assertEquals("Hello, World!", result.get(0));
 	}		
-	
-	
+		
 	/**
 	 * Generates a greeting with interpolation and an invalid configuration loaded from a map.
 	 * @throws Exception
@@ -200,56 +216,40 @@ public class StaticTextTests extends TestsBase {
 		Container<InputStream> fsc = new FileSystemContainer(new File("target/generator-tests/static-text/interpolation-with-invalid-configuration"));
 		
 		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
-		ProgressEntry pe = new ProgressEntry("Generating Generator Model Documentation", 0);
+		ProgressEntry pe = new ProgressEntry("Generating interpolated text", 0);
 		List<String> result = generator.bindContext(Context.EMPTY_CONTEXT).execute(progressMonitor.compose(pe));	
 		
 		// HTML report
 		Container<Object> container = fsc.adapt(null, encoder, null);
-		ProgressReportGenerator prg = new ProgressReportGenerator("Documentation generation", pe);
+		ProgressReportGenerator prg = new ProgressReportGenerator("Generation", pe);
 		prg.generate(container.getContainer("progress-report"), progressMonitor);				
 		
 		Assert.assertEquals(1, result.size());
 		Assert.assertEquals("Hello, World!", result.get(0));
 	}		
+
+//	interpolation-configuration-invalid.yml
+//	interpolation-configuration.yml
+//	interpolation.codegen documentation
 	
-//	/**
-//	 * Generates a greeting without interpolation. Uses a map loaded from YAML resource for configuration. Validates the map that it contains ``name`` key.
-//	 * @throws Exception
-//	 */
-//	@Test
-//	public void testBasicConfigurableValidatingGeneration() throws Exception {
-//		ValidatingModelGenerator<String> validatingModelGenerator = new ValidatingModelGenerator<>(TEST_MODELS_BASE_URI+"static-text/basic.codegen");
-//		Container<InputStream> fsc = new FileSystemContainer(new File("target/generator-tests/static-text/basic"));
-//		MutableContext mc = new SimpleMutableContext();
-//		mc.register(Container.class, fsc);
-//		mc.put("name", "World");
-//		
-//		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
-//		ProgressEntry pe = new ProgressEntry("Generating Generator Model Documentation", 0);
-//		List<String> result = validatingModelGenerator.execute(mc, progressMonitor.compose(pe));	
-//		
-//		// HTML report
-//		Container<Object> container = fsc.adapt(null, encoder, null);
-//		ProgressReportGenerator prg = new ProgressReportGenerator("Documentation generation", pe);
-//		prg.generate(container.getContainer("progress-report"), progressMonitor);				
-//		
-//		Assert.assertEquals(1, result.size());
-//		Assert.assertEquals("Hello, ${name}!", result.get(0));
-//	}	
 	
-	@Test
-	public void testBasicDocumentationGeneration() throws Exception {
-		CodegenDocumentationGenerator generator = new CodegenDocumentationGenerator("Nasdanika Hello World Codegen Model", null);
-		generator.loadModel(TEST_MODELS_BASE_URI+"static-text/basic.codegen");
-		Container<InputStream> fsc = new FileSystemContainer(new File("target/generator-model-doc/static-text/basic"));
-		ProgressMonitor progressMonitor = new PrintStreamProgressMonitor();
-		ProgressEntry pe = new ProgressEntry("Generating Generator Model Documentation", 0);
-		Container<Object> container = fsc.adapt(null, encoder, null);
-		generator.generate(container, progressMonitor.compose(pe));
-		
-		// HTML report
-		ProgressReportGenerator prg = new ProgressReportGenerator("Documentation generation", pe);
-		prg.generate(container.getContainer("progress-report"), progressMonitor);		
-	}
+	
+	
+//	arguments-no-controller-disabled.codegen
+//	arguments-no-controller.codegen
+//	cannot-convert-arguments.codegen
+//	configuration-bad.codegen
+//	configuration-list.codegen
+//	configuration-reference.codegen
+//	configuration-reference.json
+//	configuration-reference.properties
+//	configuration-reference.yml
+//	configuration.codegen
+//	context-path.codegen
+//	controller-with-arguments.codegen
+//	controller.codegen
+//	disabled.codegen
+//	wrong-controller-class.codegen
+//	wrong-number-of-controller-arguments.codegen	
 	
 }
