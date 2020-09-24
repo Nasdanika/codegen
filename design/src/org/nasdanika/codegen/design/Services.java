46df3f1914d9fba42e271d5a355fa85079f8ea15
Pavel <Pavel.Vlasov@nasdanika.org>
2020-09-24 18:47:55 -0400

    Sirius properties are resource extension specific.

--- design/src/org/nasdanika/codegen/design/Services.java
+++ design/src/org/nasdanika/codegen/design/Services.java
@@ -1,10 +1,15 @@
 package org.nasdanika.codegen.design;
 
+import org.eclipse.emf.ecore.EObject;
 import org.nasdanika.sirius.CommonServices;
 
 /**
  * The services class used by VSM.
  */
 public class Services extends CommonServices {
+	    
+    public boolean isCodegenResource(EObject self) {
+    	return isResourceExtension(self, "codegen");
+    }	
 
 }
